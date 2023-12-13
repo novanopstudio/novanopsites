d0dad1651966e157ef6e77ea8d121ed8b53bfe33
novanopstudio <novanop.studio@gmail.com>
2023-12-13 15:53:58 +0700

    chore: update favicon and fix contact form bugs

--- js/script.js
+++ js/script.js
@@ -1,77 +1,75 @@
 (function ($) {
-    'use strict';
+  "use strict";
 
-    /*-------------------------------------------------------------------------------
+  /*-------------------------------------------------------------------------------
       Animsition init
     -------------------------------------------------------------------------------*/
 
-    $('.animsition').animsition({
-        loadingClass: 'preloader',
-        loadingInner: '<div class="spinner"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>'
-    });
+  $(".animsition").animsition({
+    loadingClass: "preloader",
+    loadingInner:
+      '<div class="spinner"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>',
+  });
 
-    /*-------------------------------------------------------------------------------
+  /*-------------------------------------------------------------------------------
       Wow init
     -------------------------------------------------------------------------------*/
 
-    new WOW({ mobile: false }).init();
+  new WOW({ mobile: false }).init();
 
-
-    /*-------------------------------------------------------------------------------
+  /*-------------------------------------------------------------------------------
       Menu
     -------------------------------------------------------------------------------*/
 
-    $('.a-nav-toggle').on('click', function () {
-        if ($('html').hasClass('body-menu-opened')) {
-            $('html').removeClass('body-menu-opened').addClass('body-menu-closed');
-        } else {
-            $('html').addClass('body-menu-opened').removeClass('body-menu-closed');
-        }
-    });
+  $(".a-nav-toggle").on("click", function () {
+    if ($("html").hasClass("body-menu-opened")) {
+      $("html").removeClass("body-menu-opened").addClass("body-menu-closed");
+    } else {
+      $("html").addClass("body-menu-opened").removeClass("body-menu-closed");
+    }
+  });
 
-    /*-------------------------------------------------------------------------------
+  /*-------------------------------------------------------------------------------
       Masonry
     -------------------------------------------------------------------------------*/
 
-    $(window).on('load', function () {
-        if ($('.a-grid').length) {
-            $('.a-grid').isotope({
-                itemSelector: '.grid-item'
-            });
-            $('.a-grid-filter a').on('click', function () {
-                $(this).parents('.a-grid-filter').find('.active').removeClass('active');
-                $(this).parent().addClass('active');
-                var filterValue = $(this).attr('data-filter');
-                $('.a-grid').isotope({ filter: filterValue });
-            });
-        }
+  $(window).on("load", function () {
+    if ($(".a-grid").length) {
+      $(".a-grid").isotope({
+        itemSelector: ".grid-item",
+      });
+      $(".a-grid-filter a").on("click", function () {
+        $(this).parents(".a-grid-filter").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+        var filterValue = $(this).attr("data-filter");
+        $(".a-grid").isotope({ filter: filterValue });
+      });
+    }
 
-        if ($('.a-grid-line').length) {
-            $('.a-grid-line').isotope({
-                itemSelector: '.grid-item',
-                layoutMode: 'fitRows'
-            });
-            $('.a-grid-filter a').on('click', function () {
-                $(this).parents('.a-grid-filter').find('.active').removeClass('active');
-                $(this).parent().addClass('active');
-                var filterValue = $(this).attr('data-filter');
-                $('.a-grid-line').isotope({ filter: filterValue });
-            });
-        }
-    });
-
-}($));
-
+    if ($(".a-grid-line").length) {
+      $(".a-grid-line").isotope({
+        itemSelector: ".grid-item",
+        layoutMode: "fitRows",
+      });
+      $(".a-grid-filter a").on("click", function () {
+        $(this).parents(".a-grid-filter").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+        var filterValue = $(this).attr("data-filter");
+        $(".a-grid-line").isotope({ filter: filterValue });
+      });
+    }
+  });
+})($);
 
 /*-------------------------------------------------------------------------------
       Validation
     -------------------------------------------------------------------------------*/
 
 const isValidEmail = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-
-}
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
 
 const form = document.querySelector("form");
 const thankYou = document.querySelector(".thank-you");
@@ -79,7 +77,7 @@ const nameInput = document.querySelector('input[name="name"]');
 const emailInput = document.querySelector('input[name="_replyto"]');
 const subjectInput = document.querySelector('input[name="subject"]');
 const messageInput = document.querySelector('textarea[name="message"]');
-const submitButton = document.getElementById('submit-button');
+const submitButton = document.getElementById("submit-button");
 
 const inputs = [nameInput, emailInput, subjectInput, messageInput];
 
@@ -87,77 +85,78 @@ let isFormValid = false;
 let isValidationOn = false;
 
 const resetElm = (elm) => {
-    elm.classList.remove("error");
-    elm.nextElementSibling.classList.add("hidden");
-}
+  elm.classList.remove("error");
+  elm.nextElementSibling.classList.add("hidden");
+};
 
 const invalidateElm = (elm) => {
-    elm.classList.add("error")
-    elm.nextElementSibling.classList.remove("hidden");
-}
+  elm.classList.add("error");
+  elm.nextElementSibling.classList.remove("hidden");
+};
 
 const validateInputs = () => {
-    if (!isValidationOn) return;
+  if (!isValidationOn) return;
 
-    isFormValid = true;
-    inputs.forEach(resetElm);
+  isFormValid = true;
+  inputs.forEach(resetElm);
 
-    if (!nameInput.value) {
-        isFormValid = false;
-        invalidateElm(nameInput);
-    }
-    if (!isValidEmail(emailInput.value)) {
-        isFormValid = false;
-        invalidateElm(emailInput);
-    }
-    if (!subjectInput.value) {
-        isFormValid = false;
-        invalidateElm(subjectInput);
-    }
-    if (!messageInput.value) {
-        isFormValid = false;
-        invalidateElm(messageInput);
-    }
+  if (!nameInput.value) {
+    isFormValid = false;
+    invalidateElm(nameInput);
+  }
+  if (!isValidEmail(emailInput.value)) {
+    isFormValid = false;
+    invalidateElm(emailInput);
+  }
+  if (!subjectInput.value) {
+    isFormValid = false;
+    invalidateElm(subjectInput);
+  }
+  if (!messageInput.value) {
+    isFormValid = false;
+    invalidateElm(messageInput);
+  }
 };
 
 if (form) {
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        isValidationOn = true;
-        validateInputs();
-        if (isFormValid) {
-            submitButton.classList.add('button-loading');
-            submitButton.textContent = 'Please Wait';
-            setTimeout(sendEmail, 2000);
-            nameInput.disabled = true;
-            emailInput.disabled = true;
-            subjectInput.disabled = true;
-            messageInput.disabled = true;
-            submitButton.disabled = true;
-        }
-    });
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    isValidationOn = true;
+    validateInputs();
+    if (isFormValid) {
+      submitButton.classList.add("button-loading");
+      submitButton.textContent = "Please Wait";
+      setTimeout(sendEmail, 2000);
+      nameInput.disabled = true;
+      emailInput.disabled = true;
+      subjectInput.disabled = true;
+      messageInput.disabled = true;
+      submitButton.disabled = true;
+    }
+  });
 }
 
-inputs.forEach(input => {
-    if (input) {
-        input.addEventListener("input", () => {
-            validateInputs();
-        });
-    }
+inputs.forEach((input) => {
+  if (input) {
+    input.addEventListener("input", () => {
+      validateInputs();
+    });
+  }
 });
 
 function sendEmail(Email) {
-    const tempParams = {
-        from_name: nameInput.value,
-        email_subject: subjectInput.value,
-        reply_to: emailInput.value,
-        message: messageInput.value
-    };
+  const tempParams = {
+    from_name: nameInput.value,
+    email_subject: subjectInput.value,
+    reply_to: emailInput.value,
+    message: messageInput.value,
+  };
 
-    emailjs.send('service_wnjiac9', 'template_8xpg4sm', tempParams)
-        .then(function (res) {
-            console.log("Success", res.status);
-            form.remove();
-            thankYou.classList.remove("hidden");
-        });
-};
+  emailjs
+    .send("service_dnx56hw", "template_549l9li", tempParams)
+    .then(function (res) {
+      console.log("Success", res.status);
+      form.remove();
+      thankYou.classList.remove("hidden");
+    });
+}
--- php/contact-form-handler.php
+++ php/contact-form-handler.php
@@ -1,36 +1,38 @@
 <?php
 if (!isset($_POST['submit'])) {
-    //This page should not be accessed directly. Need to submit the form.
+    // This page should not be accessed directly. Need to submit the form.
     echo "error; you need to submit the form!";
-}
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+} else {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
 
-//Validate first
-if (empty($name) || empty($visitor_email)) {
-    echo "Name and email are mandatory!";
-    exit;
-}
+    // Validate first
+    if (empty($name) || empty($visitor_email)) {
+        echo "Name and email are mandatory!";
+        exit;
+    }
 
-if (IsInjected($visitor_email)) {
-    echo "Bad email value!";
-    exit;
-}
+    if (IsInjected($visitor_email)) {
+        echo "Bad email value!";
+        exit;
+    }
 
-$email_from = 'halo@novanop.com'; //<== update the email address
-$email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name.\n" .
-    "Here is the message:\n $message" .
+    $email_from = $visitor_email; //<== update the email address
+    $email_subject = "New Form submission";
+    $email_body = "You have received a new message from the user $name.\n" .
+        "Here is the message:\n $message";
 
     $to = "halo@novanop.com"; //<== update the email address
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-//Send the email!
-mail($to, $email_subject, $email_body, $headers);
-//done. redirect to thank-you page.
-header('Location: index.html');
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
+    // Send the email!
+    mail($to, $email_subject, $email_body, $headers);
+
+    // done. redirect to thank-you page.
+    header('Location: index.html');
+}
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
@@ -52,70 +54,4 @@ function IsInjected($str)
         return false;
     }
 }
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-
-$email_from = "Novanop.com";
-$email_body = "Name: $name \n".
-"Email: $visitor_email \n".
-"Message: $message \n";
-
-
-$to = 'halo@novanop.com';
-$headers = "From : $visitor_email \r\n";
-$headers = "Reply-to: $visitor_email \r\n";
-
-mail($to,$subject,$email_body,$headers);
-
-header("Location: index.html");
-
-
-?>*/
